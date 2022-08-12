b8bbd0aad6dac1b59c3b415b0d12b7daa937de3a
1stMonkey <The.IT.Monkey@hotmail.com>
2022-08-11 17:04:03 -0700

    Changes to comply with PS folder.

--- Workstation/Winget_Install.ps1
+++ Workstation/Winget_Install.ps1
@@ -25,6 +25,7 @@
       + New Feature
 #>
 
+
 ## The following four lines only need to be declared once in your script.
 $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes","Description."
 $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No","Description."
@@ -32,36 +33,69 @@ $cancel = New-Object System.Management.Automation.Host.ChoiceDescription "&Cance
 $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no, $cancel)
 $softwareTitle = "Microsoft.PowerShell", "Microsoft.VisualStudioCode", "Git.Git","Insecure.Nmap", "Balena.Etcher", "Microsoft.PowerToys", "Adobe.Acrobat.Reader.64-bit","Microsoft.SQLServerManagementStudio", "JanDeDobbeleer.OhMyPosh","RoyalApps.RoyalTS","Microsoft.WindowsTerminal"
 
-$title = "Fonts" 
-$message = "Do you want to download font for OhMyPosh?"
-$result = $host.ui.PromptForChoice($title, $message, $options, 1)
 
-switch ($result) {
-  0{
-    Invoke-WebRequest https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
-  }
-  1{
-    Write-Host "The user declined download"
-  }
-  2{
-    exit
-  }
-}
 
-foreach ($App in $softwareTitle) {
-  $title = "Software Installation" 
-  $message = "Do you want to install $App ?"
+
+
+
+Function OhMy{
+  $title = "Fonts" 
+  $message = "Do you want to download font for OhMyPosh?"
   $result = $host.ui.PromptForChoice($title, $message, $options, 1)
-
+  
   switch ($result) {
     0{
-     Winget Install $App
+      Invoke-WebRequest https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
     }
     1{
-      Write-Host "The user declined installation"
+      Write-Host "The user declined download"
     }
     2{
       exit
     }
   }
 }
+
+
+Function WingetInstallSoftware{
+  foreach ($App in $softwareTitle) {
+    $title = "Software Installation" 
+    $message = "Do you want to install $App ?"
+    $result = $host.ui.PromptForChoice($title, $message, $options, 1)
+  
+    switch ($result) {
+      0{
+       Winget Install $App
+      }
+      1{
+        Write-Host "The user declined installation"
+      }
+      2{
+        exit
+      }
+    }
+  }
+}
+
+function WingetUpgradeall {
+  if (Get-ScheduledTask -TaskName WingetUpgrade){
+    Write-Host "Schedule task exists"
+
+ 
+  }
+ else {
+     $Mypath = '-NoLogo -NoProfile  Winget upgrade --all'
+     $Action = New-ScheduledTaskAction -Execute 'pwsh.exe' -Argument $Mypath
+     $Trigger = New-ScheduledTaskTrigger -Weekly -WeeksInterval 2 -DaysOfWeek Monday -At 8am
+     $Settings = New-ScheduledTaskSettingsSet
+     $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
+     Register-ScheduledTask -TaskName 'WingetUpgrade' -InputObject $Task 
+ }
+
+
+  
+}
+
+WingetUpgradeall
+OhMy
+WingetInstallSoftware
